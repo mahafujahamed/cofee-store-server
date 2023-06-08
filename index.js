aaaa7b6a87d484dde8edc97c3326adfdcf01cd4d
Mahafuj-Ahamed <mahafujahamed068@gamil.com>
2023-06-08 17:55:57 +0600

    retrive server pinged

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.blgp36b.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -27,12 +28,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const coffeeCollection = client.db("coffeeDB").collection("coffeeList");
+
+
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log('adding new coffee: ', newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -40,11 +52,11 @@ run().catch(console.dir);
 
 // routes
 app.get('/', (req, res) => {
-    res.send('Cofee making server is running');
+  res.send('Cofee making server is running');
 }
 );
 
 app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+  console.log(`Server is running on port: ${port}`);
 }
 );
